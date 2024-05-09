f57fb279b92bae72b600734017286acb0f735442
Victor <vdiaz.2192@gmail.com>
2024-05-09 17:54:23 -0500

    merge develop

--- src/app/modules/e-commerce/layout.component.ts
+++ src/app/modules/e-commerce/layout.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import {
   MatDrawer,
   MatDrawerContainer,
   MatDrawerContent,
 } from '@angular/material/sidenav'; // For compatibility with older Angular Material versions
-import { MatToolbar } from '@angular/material/toolbar';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MainContainerComponent } from '@components/main-container/main-container.component';
 import { CabeceraComponent } from '../../shared/components/cabecera/cabecera.component';
 
@@ -35,7 +35,6 @@ import { CabeceraComponent } from '../../shared/components/cabecera/cabecera.com
     MainContainerComponent,
     RouterLink,
     RouterLinkActive,
-    RouterOutlet,
     CabeceraComponent,
   ],
 })
--- src/app/modules/e-commerce/pages/inicio/inicio.component.ts
+++ src/app/modules/e-commerce/pages/inicio/inicio.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { product_List } from './Inicio.type';
+import { Router } from '@angular/router';
+import { ecommerceService } from '@ecommerce/e-commerce.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-inicio',
@@ -22,12 +21,61 @@ export interface Tile {
     MatCardModule,
     MatIconModule,
     MatGridListModule,
+    CarouselModule
+
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss',
 })
-export class InicioComponent {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
+
+export class InicioComponent implements OnInit {
+  productos: product_List[] = []
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    autoplay: true,
+    navText: ['Anterior', 'Siguiente'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
+  constructor(private router: Router, private _ecommerceService: ecommerceService) {
+
+  }
+
+  ngOnInit(): void {
+    this.fngetList()
+  }
+
+  fngetList() {
+    this._ecommerceService.getProducts().subscribe({
+      next: (res) => {
+
+        this.productos = res;
+
+        console.log(this.productos)
+      },
+      error: (e: HttpErrorResponse) => {
+        console.log('Error :', e)
+        return
+      }
+    });
+  }
 }
