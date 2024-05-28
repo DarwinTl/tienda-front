9d773000ef817807fb258488b895f4285e27c0af
MSiqueros <maycolsiqueros584@gmail.com>
2024-05-28 00:51:08 -0500

    28/05/2024

--- src/app/modules/e-commerce/layout.routing.ts
+++ src/app/modules/e-commerce/layout.routing.ts
@@ -3,6 +3,14 @@ import { menuResolve } from '@shared/guards/menu.resolve';
 
 const routes: Routes = [
   {
+    path: 'inicio',
+    title: 'Pagina Inicio | Don Pepe SuperMarket',
+    loadComponent: () =>
+      import('@ecommerce/pages/inicio/inicio.component').then(
+        (c) => c.InicioComponent,
+      ),
+  },
+  {
 
     path: '',
     resolve: { menu: menuResolve },
